d5288c1f491e4f47f71e1601b0de7b9cd94f91e0
3116005132 <540026837@qq.com>
2018-10-23 00:12:13 +0800

    add structure

--- 服务器/Server.py
+++ 服务器/Server.py
@@ -1,37 +1,49 @@
 import socket
+import pickle
+import InfoStructure
+import threading
 
 class UDP_ForWard():
     def __init__(self):
         self.addr_port = ('127.0.0.1', 10086)
-
-    def receive(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.bind(('127.0.0.1', 10086))
-
-        while True:
-            data, addr = s.recvfrom(1024)
-            print('Received from %s:%s.' % addr)
-
-            send_to = ('127.0.0.1', 10010)
-            s.sendto(data, send_to)
-            print('finish send.')
 
     def BuiltSocket(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.bind(self.addr_port)
         return s
 
-    def Forward(self):
+    def GetInfo(self):
         UDP_socket = self.BuiltSocket()
         while True:
-            data, addr = UDP_socket.recvfrom(1024)
-            print('Received from %s:%s.' % addr)
+            data, addr = UDP_socket.recvfrom(2048)
+            print("Connection : ", addr)
+            data_structure = pickle.loads(data)
 
+            if data_structure.operation_num == 0:
+                # Register
+                pass
+
+            elif data_structure.operation_num == 1:
+                # Login
+                pass
+
+            elif data_structure.operation_num == 2:
+                # Chatting
+                threading.Thread(target=self.Forward, args=(UDP_socket, data_structure, )).start()
+                # self.Forward(UDP_socket, data_structure)
+
+            elif data_structure.operation_num == 3:
+                # Exit
+                pass
+
+    def Forward(self, UDP_socket, data_structure):
+            print('Received from :%s.' % data_structure.username)
+            data = pickle.dumps(data_structure)
             # 强行实现双向转发
-            if addr == ('127.0.0.1', 10000):
+            if data_structure.userIP == ('127.0.0.1:10000'):
                 UDP_socket.sendto(data , ('127.0.0.1', 10001))
             else:
                 UDP_socket.sendto(data, ('127.0.0.1', 10000))
 
 s = UDP_ForWard()
-s.Forward()
+s.GetInfo()
